e5ea43156137c55ac53669e61f420d8006abcbfe
aristiguetam <aristiguetam97@gmail.com>
2023-01-21 16:51:44 -0500

    Agregando el home y el paginado PA

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,3 +1,27 @@
+'use client';
+import { useState } from 'react';
+import styles from '../app/page.module.css';
+import Cardjob from '../components/Cardjob';
+import prueba from '../components/contenido';
+import BasicPagination from '../components/Paginate/Pagination';
+
 export default function Home() {
-  return <h1>Este es el home</h1>;
+  const [paginate, setPaginate] = useState(prueba.slice(0, 5));  
+  return (
+    <main className={`container`}>
+      <section className={`${styles['container_grid']}`}>
+        {paginate.map((x) => (
+          <Cardjob
+            key={x.id.toString()}
+            nombre={x.nombre}
+            imagen={x.imagen}
+            texto={x.texto}
+            precio={x.dinero}
+            hora={x.hora}
+          />
+        ))}
+      </section>
+      <BasicPagination prueba={prueba} setPaginate={setPaginate} />
+    </main>
+  );
 }
