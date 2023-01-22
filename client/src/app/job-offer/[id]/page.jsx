e48ca10c50845cf2ee0f86cffc6d2683e5ac147a
Dariana Rengifo <darianarengifo@gmail.com>
2023-01-22 14:20:28 -0300

    [to-do-detail] feat (client): add comments on what needs to be done in the detail of a job offer.

--- client/src/app/job-offer/[id]/page.jsx
+++ client/src/app/job-offer/[id]/page.jsx
@@ -1,93 +1,56 @@
-import style from "./page.module.css";
-
-// ESTA simulacionApi DEBE ELIMINARSE Y TRAER LAS CATEGORIAS DE LA API.
-const simulacionApi = [
-  {
-    id: 1,
-    category: "Jardinería",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus.",
-    tasks: ["Tarea 1", "Tarea 2", "Tarea 3", "Tarea 4"],
-    time: 6,
-    remuneration: 20,
-    negotiable: false,
-    location: "Mendoza, Argentina",
-  },
-  {
-    id: 2,
-    category: "Carpintería",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus.",
-    tasks: ["Tarea 1", "Tarea 2", "Tarea 3", "Tarea 4", "Tarea 5"],
-    time: 8,
-    remuneration: 30,
-    negotiable: true,
-    location: "Caracas, Venezuela",
-  },
-  {
-    id: 3,
-    category: "Pintura",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus.",
-    tasks: ["Tarea 1", "Tarea 2", "Tarea 3"],
-    time: 7,
-    remuneration: 25,
-    negotiable: false,
-    location: "Lima, Perú",
-  },
-];
+import style from './page.module.css';
 
 export default function JobOffer({ params }) {
   const { id } = params;
-  // EN jobOffer SE DEBEN GUARDAR DEL TRABAJO, TRAIDOS DE LA API (lo demás no cambia)
-  const jobOffer = simulacionApi.find((job) => `${job.id}` === `${id}`);
+  // TO DO 1/1: Sustituir el valor de jobOffer por el detalle real de la oferta de trabajo, utilizando el id que se recibe por parámetro.
+  const jobOffer = 'detalle de la oferta de trabajo';
   return (
     <>
       {!jobOffer ? (
         <h1>Page not Found</h1>
       ) : (
-        <div className='container'>
+        <div className="container">
           <h1>{jobOffer.category}</h1>
           <p>{jobOffer.description}</p>
-          <h4 className={`${style["h4-task"]}`}>Tareas a realizar:</h4>
-          <ul className={`${style["ul-task"]}`}>
+          <h4 className={`${style['h4-task']}`}>Tareas a realizar:</h4>
+          <ul className={`${style['ul-task']}`}>
             {jobOffer.tasks.map((task, index) => {
               return (
-                <li key={`task-${index}`} className={`${style["li-task"]}`}>
+                <li key={`task-${index}`} className={`${style['li-task']}`}>
                   {task}
                 </li>
               );
             })}
           </ul>
-          <div className={`${style["container-features"]}`}>
-            <div className={`${style["div-time"]}`}>
+          <div className={`${style['container-features']}`}>
+            <div className={`${style['div-time']}`}>
               <img
-                src='https://cdn-icons-png.flaticon.com/512/5561/5561832.png'
-                alt='Time'
+                src="https://cdn-icons-png.flaticon.com/512/5561/5561832.png"
+                alt="Time"
               />
               <h4>Duración estimada:</h4>
               <p>{jobOffer.time} hs</p>
             </div>
-            <div className={`${style["div-remuneration"]}`}>
+            <div className={`${style['div-remuneration']}`}>
               <img
-                src='https://cdn-icons-png.flaticon.com/512/9420/9420018.png'
-                alt='Remuneration'
+                src="https://cdn-icons-png.flaticon.com/512/9420/9420018.png"
+                alt="Remuneration"
               />
               <h4>Remuneración:</h4>
               <p>$ {jobOffer.remuneration}</p>
             </div>
-            <div className={`${style["div-negotiable"]}`}>
+            <div className={`${style['div-negotiable']}`}>
               <img
-                src='https://cdn-icons-png.flaticon.com/512/4878/4878245.png'
-                alt='Negotiable'
+                src="https://cdn-icons-png.flaticon.com/512/4878/4878245.png"
+                alt="Negotiable"
               />
               <h4>Negociable:</h4>
               {jobOffer.negotiable ? <p>Si.</p> : <p>No.</p>}
             </div>
-            <div className={`${style["div-location"]}`}>
+            <div className={`${style['div-location']}`}>
               <img
-                src='https://cdn-icons-png.flaticon.com/512/3082/3082383.png'
-                alt='Location'
+                src="https://cdn-icons-png.flaticon.com/512/3082/3082383.png"
+                alt="Location"
               />
               <h4>Ubcación:</h4>
               <p>{jobOffer.location}.</p>
