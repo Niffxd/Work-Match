f4ac96ed950d973a14ce7e64dfe37521444a7c22
DeGssoft <diegoezequielguillen@gmail.com>
2023-01-21 21:32:01 -0300

    [PRUEB-22] fix (model/user): format Pascal Case for sequelize

--- api/src/models/user.model.js
+++ api/src/models/user.model.js
@@ -4,7 +4,7 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define(
-    'user',
+    'User',
     {
       id: {
         type: DataTypes.INTEGER,
