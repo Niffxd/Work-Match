127e154bb512480144224ad92efe0edea87cbfd3
Dariana Rengifo <darianarengifo@gmail.com>
2023-01-22 13:49:22 -0300

    [sintaxis-styles] feat (client): change syntax of selectors in variables.css.

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,10 +1,10 @@
 import '../styles/normalize.css';
 import '../styles/index.css';
-import Footer from '../components/Footer/Footer.jsx'
+import Footer from '../components/Footer/Footer.jsx';
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html className="html" lang="en">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
       <head />
       <body>
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
