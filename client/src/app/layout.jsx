cd978180255c63f054465029a1767198d474ad5d
Niffxd <nicosanchez673@gmail.com>
2023-01-22 18:33:06 -0300

    [PRUEB-55] feat (client/src/app): update layout.jsx adding Navigation component to general layout

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,18 +1,20 @@
 import '../styles/normalize.css';
 import '../styles/index.css';
-import Footer from '../components/Footer/Footer.jsx';
+import Footer from '../components/Footer/Footer.jsx'
+import Navigation from '../components/Navigation/Navigation.jsx'
 
 export default function RootLayout({ children }) {
   return (
-    <html className="html" lang="en">
+    <html className='html' lang="en">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
       <body>
+        <Navigation/>
         {children}
-        <Footer />
+        <Footer/>
       </body>
     </html>
   );
