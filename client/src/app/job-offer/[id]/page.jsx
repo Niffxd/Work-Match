7507b98b61c9299eb7cb0e784a595eae21e16bd3
Dariana Rengifo <darianarengifo@gmail.com>
2023-01-23 00:08:25 -0300

    [detail-job-offer] feat (client): update detail of a job offer.

--- client/src/app/job-offer/[id]/page.jsx
+++ client/src/app/job-offer/[id]/page.jsx
@@ -1,18 +1,21 @@
+import { getProject } from '@/utils/api/project';
 import style from './page.module.css';
 
-export default function JobOffer({ params }) {
+export default async function JobOffer({ params }) {
   const { id } = params;
   // TO DO 1/1: Sustituir el valor de jobOffer por el detalle real de la oferta de trabajo, utilizando el id que se recibe por parámetro.
-  const jobOffer = 'detalle de la oferta de trabajo';
+  const jobOffer = await getProject(parseInt(id));
+  console.log(jobOffer);
   return (
     <>
       {!jobOffer ? (
         <h1>Page not Found</h1>
       ) : (
         <div className="container">
-          <h1>{jobOffer.category}</h1>
+          <h1>{jobOffer.title}</h1>
+          <h4>Descripción del trabajo:</h4>
           <p>{jobOffer.description}</p>
-          <h4 className={`${style['h4-task']}`}>Tareas a realizar:</h4>
+          {/* <h4 className={`${style['h4-task']}`}>Tareas a realizar:</h4>
           <ul className={`${style['ul-task']}`}>
             {jobOffer.tasks.map((task, index) => {
               return (
@@ -21,23 +24,23 @@ export default function JobOffer({ params }) {
                 </li>
               );
             })}
-          </ul>
+          </ul> */}
           <div className={`${style['container-features']}`}>
-            <div className={`${style['div-time']}`}>
+            {/* <div className={`${style['div-time']}`}>
               <img
                 src="https://cdn-icons-png.flaticon.com/512/5561/5561832.png"
                 alt="Time"
               />
               <h4>Duración estimada:</h4>
               <p>{jobOffer.time} hs</p>
-            </div>
+            </div> */}
             <div className={`${style['div-remuneration']}`}>
               <img
                 src="https://cdn-icons-png.flaticon.com/512/9420/9420018.png"
                 alt="Remuneration"
               />
               <h4>Remuneración:</h4>
-              <p>$ {jobOffer.remuneration}</p>
+              <p>$ {jobOffer.budget}</p>
             </div>
             <div className={`${style['div-negotiable']}`}>
               <img
@@ -45,7 +48,7 @@ export default function JobOffer({ params }) {
                 alt="Negotiable"
               />
               <h4>Negociable:</h4>
-              {jobOffer.negotiable ? <p>Si.</p> : <p>No.</p>}
+              {jobOffer.agreement ? <p>Si.</p> : <p>No.</p>}
             </div>
             <div className={`${style['div-location']}`}>
               <img
@@ -53,7 +56,7 @@ export default function JobOffer({ params }) {
                 alt="Location"
               />
               <h4>Ubcación:</h4>
-              <p>{jobOffer.location}.</p>
+              <p>{jobOffer.address}.</p>
             </div>
           </div>
         </div>
