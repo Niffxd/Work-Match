d64e188dbe2a2df77acaeb936d14c955fb28dd8a
Niffxd <nicosanchez673@gmail.com>
2023-01-25 16:26:50 -0300

    view only but it shows up as changes

--- api/src/app.js
+++ api/src/app.js
@@ -34,7 +34,7 @@ server.use((err, req, res, next) => {
   // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
-  // console.error(err);
+  console.error(err);
   res.status(status).send(message);
 });
 
