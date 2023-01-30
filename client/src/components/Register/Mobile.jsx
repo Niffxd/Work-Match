c3de4ae7838af0695922403a1f286e7c2a978141
Niffxd <nicosanchez673@gmail.com>
2023-01-30 05:51:08 -0300

    adding register

--- client/src/components/Register/Mobile.jsx
+++ client/src/components/Register/Mobile.jsx
@@ -4,55 +4,55 @@ import NextButton from './NextButton.jsx'
 import AddButton from './AddButton.jsx'
 import user from '../../assets/images/user.png'
 import style from './register.module.css'
-import './style.css'
 
 export default function Mobile() {
-  const [validateName, setValidateName] = useState(false)
-  const [validateLastname, setValidateLastname] = useState(false)
+  const [validateEmail, setValidateEmail] = useState(false)
+  const [validatePassword, setValidatePassword] = useState(false)
   const [validate, setValidate] = useState(false)
 
   const [ preForm, setPreform ] = useState({
-    name: "",
-    lastname: ""
+    email: "",
+    password: ""
   })
 
   const handleNameChange = (event) => {
-    setPreform({...preForm, name: event.target.value })
-    if(event.target.value.length > 0) setValidateName(true)
-    else setValidateName(false)
+    setPreform({...preForm, email: event.target.value })
+    if(event.target.value.length > 0) setValidateEmail(true)
+    else setValidateEmail(false)
 
-    if(validateName && validateLastname) setValidate(true)
+    if(validateEmail && validatePassword) setValidate(true)
     else setValidate(false)
   }
 
   const handleLastnameChange = (event) => {
-    setPreform({...preForm, lastname: event.target.value })
-    if(event.target.value.length > 0) setValidateLastname(true)
-    else setValidateLastname(false)
+    setPreform({...preForm, password: event.target.value })
+    if(event.target.value.length > 0) setValidatePassword(true)
+    else setValidatePassword(false)
 
-    if(validateName && validateLastname) setValidate(true)
+    if(validateEmail && validatePassword) setValidate(true)
     else setValidate(false)
   }
 
   return (
     <div className={`${style['mobile']}`}>
-    <div className={`${style['add-photo-container']}`}>
-      <div className={`${style['photo-container']}`}>
-        <img
-          className={`${style['photo-profile']}`}
-          src={user}
-          width={100}
-          height={100}
-          alt="profile" />
-      </div>
+      <div className={`${style['add-photo-container']}`}>
+        <div className={`${style['photo-container']}`}>
+          <img
+            className={`${style['photo-profile']}`}
+            src={user}
+            width={100}
+            height={100}
+            alt="profile"
+          />
+        </div>
       <AddButton/>
     </div>
     <div className={`${style['info']}`}>
-      <label htmlFor="name">Nombre:</label>
-      <input name='name' type="text" onChange={handleNameChange} />
-      <label htmlFor="lastname">Apellido:</label>
-      <input name='lastname' type="text" onChange={handleLastnameChange} />
-      <NextButton validate={validate}/>
+      <label htmlFor="email">Email:</label>
+      <input name='email' type="email" onChange={handleNameChange} />
+      <label htmlFor="password">Contraseña:</label>
+      <input name='password' type="password" onChange={handleLastnameChange} />
+      <NextButton validate={validate} preForm={preForm} />
       <div className={`${style['login']}`}>
         <p>¿Ya tienes cuenta?</p>
         <Link to="/login">
--- client/src/components/Register/NextButton.jsx
+++ client/src/components/Register/NextButton.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import style from './buttons.module.css'
 
 export default function NextButton(props) {
+  const state = props.preForm
+
   const showModal = () => {
     document.getElementById('completeProfile').showModal()
     document.getElementById('completeProfile').classList.add('showModal')
@@ -12,13 +14,16 @@ export default function NextButton(props) {
     document.getElementById('completeProfile').classList.remove('showModal')
   }
 
-  return props.validate ? (
-    <Link to='/register/complete'>
-      <button className={`${style['next-button']}`}>
-          Siguiente
-      </button>
-    </Link>
-  ) : 
+  return props.validate
+    ? <Link to={{
+        pathname: '/register/complete',
+        state: state
+      }}>
+        <button className={`${style['next-button']}`}>
+            Siguiente
+        </button>
+      </Link>
+    : 
   <>
     <dialog id='completeProfile'>
       <h3>Ingrese los datos requeridos.</h3>
