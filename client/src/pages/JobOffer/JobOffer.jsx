3bc088d357195dc0fc5fc3c8ee53c22db4f3f87b
Dariana Rengifo <darianarengifo@gmail.com>
2023-02-01 15:59:09 -0300

    [update-home] feat (client): update home.

--- client/src/pages/JobOffer/JobOffer.jsx
+++ client/src/pages/JobOffer/JobOffer.jsx
@@ -1,60 +1,178 @@
+import { use } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { getProjectId } from "../../redux/actions/projectActions";
+import { useParams, useHistory } from "react-router-dom";
+import {
+  deleteProjects,
+  getOwner,
+  getProjectId,
+} from "../../redux/actions/projectActions";
+import { getUserId, userApplication } from "../../redux/actions/userActions";
+
 import style from "./jobOffer.module.css";
 
 export default function JobOfferDetail() {
   const { id } = useParams();
+  const history = useHistory();
   const dispatch = useDispatch();
   const jobOfferState = useSelector((state) => state.project);
-  const { oneProject } = jobOfferState;
-  const categoriesState = useSelector((state) => state.categories);
-  const { categories } = categoriesState;
-  const jobCategory = categories.find(
-    (categ) => categ.id === oneProject.category
+  const { oneProject, owner } = jobOfferState;
+  const userState = useSelector((state) => state.user);
+  const { user } = userState;
+  let application =
+    user.Projects &&
+    user.Projects.find((project) => project.id === oneProject.id);
+  const addressState = useSelector((state) => state.address);
+  const { states } = addressState;
+  const addressJob = states.find(
+    (state) => state.id === parseInt(oneProject.state)
   );
-  console.log(jobCategory);
-  console.log(oneProject);
+
   useEffect(() => {
     dispatch(getProjectId(id));
   }, []);
 
+  useEffect(() => {
+    dispatch(getOwner(oneProject.owner));
+    dispatch(getUserId(2));
+  }, [oneProject]);
+
+  const editHandler = (event) => {
+    event.preventDefault();
+    history.push(`/create-job-offer`);
+  };
+
+  const deleteHandler = (event) => {
+    event.preventDefault();
+    dispatch(deleteProjects(oneProject.id));
+    history.push(`/`);
+  };
+
+  const applicationHandler = (event) => {
+    event.preventDefault();
+    dispatch(
+      userApplication({
+        project: oneProject.id,
+        user: user.id,
+        owner: owner.id,
+      })
+    );
+    application =
+      user.Projects &&
+      user.Projects.find((project) => project.id === oneProject.id);
+  };
+
   return (
-    <article className='container'>
-      {!oneProject ? (
+    <article className={`container ${style["job-offer-container"]}`}>
+      {!oneProject && !owner ? (
         <h1>Page not Found</h1>
       ) : (
         <>
-          <h1>{jobCategory.name}</h1>
-          <h4>Descripción del puesto:</h4>
-          <p>{oneProject.description}</p>
-          <section className={`${style["container-features"]}`}>
-            <div className={`${style["div-budget"]}`}>
+          <section className={`${style["info-container"]}`}>
+            <div className={`${style["data-container"]}`}>
+              {/* Photo profile */}
+              <div className={`${style["photo-container"]}`}>
+                <img
+                  className={`${style["photo-profile"]}`}
+                  src='https://i.pinimg.com/736x/b5/49/41/b5494197b2d462c940f88988b203d290.jpg'
+                  alt='Photo profile.'
+                />
+                {/* <img
+              className={`${style["photo-profile"]}`}
+              src={owner.image}
+              alt='Photo profile'
+            /> */}
+              </div>
+              {/* Name */}
+              <p className={`${style["user-name"]}`}>{owner.name}</p>
+              {/* Date */}
+              <p className={`${style["date"]}`}>
+                {new Date(oneProject.updatedAt).getDate()}/
+                {new Date(oneProject.updatedAt).getMonth()}/
+                {new Date(oneProject.updatedAt).getFullYear()}
+              </p>
+            </div>
+            <h4>{oneProject.Category && oneProject.Category.name}</h4>
+            <p>{oneProject.description}</p>
+            {oneProject.information && (
+              <>
+                <h4>Detalles:</h4>
+                <p>{oneProject.information}</p>
+              </>
+            )}
+
+            <section className={`${style["container-features"]}`}>
+              {oneProject.estimated && (
+                <>
+                  <img
+                    className={`${style["image-time"]}`}
+                    src='https://cdn-icons-png.flaticon.com/512/3936/3936550.png'
+                    alt='time estimated'
+                  />
+                  <div className={`${style["div-time"]}`}>
+                    <h4>Duración estimada:</h4>
+                    <p>{oneProject.estimated}h</p>
+                  </div>
+                </>
+              )}
               <img
+                className={`${style["image-budget"]}`}
                 src='https://cdn-icons-png.flaticon.com/512/9420/9420018.png'
                 alt='budget'
               />
-              <h4>Remuneración:</h4>
-              <p>$ {oneProject.budget}</p>
-            </div>
-            <div className={`${style["div-agreement"]}`}>
+              <div className={`${style["div-budget"]}`}>
+                <h4>Remuneración:</h4>
+                <p>{oneProject.budget} ARS</p>
+              </div>
               <img
+                className={`${style["image-agreement"]}`}
                 src='https://cdn-icons-png.flaticon.com/512/4878/4878245.png'
                 alt='agreement'
               />
-              <h4>Negociable:</h4>
-              {oneProject.agreement ? <p>Si.</p> : <p>No.</p>}
-            </div>
-            {/* <div className={`${style["div-address"]}`}>
+              <div className={`${style["div-agreement"]}`}>
+                <h4>Negociable:</h4>
+                {oneProject.agreement ? <p>Si.</p> : <p>No.</p>}
+              </div>
               <img
+                className={`${style["image-address"]}`}
                 src='https://cdn-icons-png.flaticon.com/512/3082/3082383.png'
                 alt='address'
               />
-              <h4>Ubcación:</h4>
-              <p>{}.</p>
-            </div> */}
+              <div className={`${style["div-address"]}`}>
+                <h4>Ubcación:</h4>
+                <p>{addressJob && addressJob.name}, Argentina.</p>
+              </div>
+            </section>
           </section>
+          {user.Projects &&
+            application &&
+            application.Bid.owner === application.Bid.user && (
+              <div className='buttons-container'>
+                <button className='button-green' onClick={editHandler}>
+                  Editar
+                </button>
+                <button className='button-red' onClick={deleteHandler}>
+                  Eliminar
+                </button>
+              </div>
+            )}
+          {user.Projects &&
+            application &&
+            application.Bid.owner !== application.Bid.user && (
+              <div className={`${style["application"]}`}>
+                <img
+                  className={`icon-green ${style["check-icon"]}`}
+                  src='https://cdn-icons-png.flaticon.com/512/87/87932.png'
+                  alt='Check'
+                />
+                <h3>Postulado</h3>
+              </div>
+            )}
+          {user.Projects && !application && (
+            <button className='button-green' onClick={applicationHandler}>
+              Postularme
+            </button>
+          )}
         </>
       )}
     </article>
