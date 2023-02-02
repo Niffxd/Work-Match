554b3f37c2b0c562d09e8936f37d2cc4756f1f6a
dan <valenciadaniel8022015@gmail.com>
2023-02-02 08:32:35 -0500

    (api)ordenar resultado de publicaciones y pequeño error del back solucionado

--- api/src/services/project.service.js
+++ api/src/services/project.service.js
@@ -12,6 +12,9 @@ async function read(id, query) {
   var data = id
     ? await Project.findAll({
         where: { id: id },
+        order: [
+          ['id', 'DESC'],
+      ],
         include: [
           {
             model: Category,
@@ -20,6 +23,9 @@ async function read(id, query) {
         ],
       })
     : await Project.findAll({
+      order: [
+        ['id', 'DESC'],
+        ],
         include: [
           {
             model: Category,
@@ -236,6 +242,9 @@ async function read(id, query) {
       meta,
     };
     data = await Project.findAll({
+      order: [
+        ['id', 'DESC'],
+        ],
       include: [
         {
           model: Category,
@@ -271,6 +280,9 @@ async function readByUser(id, query) {
       })
     : await Project.findAll({
         where: { owner: id },
+        order: [
+          ['id', 'DESC'],
+          ],
         include: [
           {
             model: User,
@@ -313,140 +325,21 @@ async function readByPostulations(id, query) {
 }
 
 async function create(project) {
-  // let message;
-  // const {
-  //   title,
-  //   description,
-  //   direction,
-  //   status,
-  //   estimatedTime,
-  //   renumerations,
-  //   agreement,
-  // } = project;
-  // // job title must be just strings
-  // const onlyLettersPattern = /^[a-zA-Z0-9_ ]*$/;
-  // const noSpecialCharacters = /[^a-zA-Z0-9 ]/;
-  // const justNumbers = /^[0-9]*$/;
-  // const jobs = {
-  //   title,
-  //   description,
-  //   direction,
-  //   status,
-  //   estimatedTime,
-  //   renumerations,
-  //   agreement,
-  // };
-  // if (
-  //   !title ||
-  //   !description ||
-  //   !direction ||
-  //   status.length === 0 ||
-  //   !estimatedTime ||
-  //   !renumerations ||
-  //   agreement.length === 0
-  // ) {
-  //   message = 'please , fill all data';
-  // } else if (!title.match(onlyLettersPattern)) {
-  //   message = 'No special characters and no numbers allowed for title, please!';
-  // }
-  // // desciption length maximun 500 characters
-  // else if (description.length > 500) {
-  //   message = 'maximum 500 characters for description!!';
-  // }
-  // // agreement can be just boolean
-  // else if (typeof agreement !== 'boolean' || typeof status !== 'boolean') {
-  //   message = 'just boolean allowed for agreement and status!';
-  // }
-  // // estimated time can not have special characters
-  // else if (estimatedTime.match(noSpecialCharacters)) {
-  //   message = 'No special characters allowed for estimated time, please!';
-  // } else if (!String(renumerations).match(justNumbers)) {
-  //   message = 'just numbers allowed, please!';
-  // }
-  // if (!message) {
-  //   const result = Projects.create(jobs);
-  //   message = 'project created successfully';
-  // }
-  // return { message };
+
 
   const data = await Project.create(project);
   const bids = {
     project: data.id,
     user: data.owner,
     deleted: project.deleted,
+    owner:data.owner
   };
+  console.log(bids)
   await Bid.create(bids);
   return data;
 }
 
 async function update(project) {
-  // let message;
-  // const projectDb = await Projects.findOne({
-  //   where: {
-  //     id,
-  //   },
-  // });
-  // if (projectDb) {
-  //   const {
-  //     title,
-  //     description,
-  //     direction,
-  //     status,
-  //     estimatedTime,
-  //     renumerations,
-  //     agreement,
-  //   } = project;
-  //   const onlyLettersPattern = /^[a-zA-Z0-9_ ]*$/;
-  //   const noSpecialCharacters = /[^a-zA-Z0-9 ]/;
-  //   const justNumbers = /^[0-9]*$/;
-  //   const jobs = {
-  //     title,
-  //     description,
-  //     direction,
-  //     status,
-  //     estimatedTime,
-  //     renumerations,
-  //     agreement,
-  //   };
-  //   if (
-  //     !title ||
-  //     !description ||
-  //     !direction ||
-  //     status.length === 0 ||
-  //     !estimatedTime ||
-  //     !renumerations ||
-  //     agreement.length === 0
-  //   ) {
-  //     message = 'please , fill all data';
-  //   } else if (!title.match(onlyLettersPattern)) {
-  //     message =
-  //       'No special characters and no numbers allowed for title, please!';
-  //   }
-  //   // desciption length maximun 500 characters
-  //   else if (description.length > 500) {
-  //     message = 'maximum 500 characters for description!!';
-  //   }
-  //   // agreement can be just boolean
-  //   else if (typeof agreement !== 'boolean' || typeof status !== 'boolean') {
-  //     message = 'just boolean allowed for agreement and status!';
-  //   }
-  //   // estimated time can not have special characters
-  //   else if (estimatedTime.match(noSpecialCharacters)) {
-  //     message = 'No special characters allowed for estimated time, please!';
-  //   } else if (!String(renumerations).match(justNumbers)) {
-  //     message = 'just numbers allowed, please!';
-  //   }
-  //   if (!message) {
-  //     const result = await Projects.update(jobs, {
-  //       where: { id },
-  //     });
-
-  //     message = 'project updated succesfully';
-  //   }
-  // } else {
-  //   message = 'id does not found';
-  // }
-  // return { message };
   const { id } = project;
 
   return Project.update(project, {
@@ -457,18 +350,7 @@ async function update(project) {
 }
 
 async function remove(id) {
-  // let message;
-  // const projectDb = await Projects.destroy({
-  //   where: {
-  //     id,
-  //   },
-  // });
-  // if (projectDb) {
-  //   message = 'project deleted successfully';
-  // } else {
-  //   message = 'Error in deleting project';
-  // }
-  // return { message };
+
   const result = await Project.update(
     {
       deleted: true,
