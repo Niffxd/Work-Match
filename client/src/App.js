3a9f36d8d81165ebd111361ee6cd0981aa42b53e
Mateo Colombatti <mateo.rng@gmail.com>
2023-02-05 22:46:05 -0300

    [PRUEB-106] feat (terms): Correcciones en Footer y agregado de Route en App.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import Loading from "./components/Register/Loading";
 import { useAuth0 } from "@auth0/auth0-react";
 import EditJobOffer from "./pages/EditJobOffer/EditJobOffer";
 import AlertMessage from "./components/AlertMessage/AlertMessage";
+import TermsYCopyright from "./pages/TermsYCopyright/TermsYCopyright";
 
 axios.defaults.baseURL = "http://localhost:3001";
 
@@ -32,29 +33,30 @@ function App() {
       <AlertMessage />
       <Navigation />
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/home'>
-          <Redirect to='/' />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home">
+          <Redirect to="/" />
         </Route>
-        <Route exact path='/job-offer/:id' component={JobOfferDetail} />
-        <Route exact path='/about-us' component={AboutUs} />
-        <Route exact path='/Login' component={Login} />
-        <Route exact path='/register' component={Register} />
-        <Route exact path='/register/complete' component={PostUser} />
-        <Route exact path='/create-job-offer' component={CreateJobOffer} />
-        <Route exact path='/edit-job-offer' component={EditJobOffer} />
-        <Route exact path='/my-profile' component={DashboardUser} />
+        <Route exact path="/job-offer/:id" component={JobOfferDetail} />
+        <Route exact path="/about-us" component={AboutUs} />
+        <Route exact path="/Login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/register/complete" component={PostUser} />
+        <Route exact path="/create-job-offer" component={CreateJobOffer} />
+        <Route exact path="/edit-job-offer" component={EditJobOffer} />
+        <Route exact path="/my-profile" component={DashboardUser} />
         <Route
           exact
-          path='/my-profile/employee/:type'
+          path="/my-profile/employee/:type"
           component={EmployeeProfile}
         />
         <Route
           exact
-          path='/my-profile/employer/:type'
+          path="/my-profile/employer/:type"
           component={EmployerProfile}
         />
-        <Route path='*' component={Error404} />
+        <Route exact path="/terms-and-copyright" component={TermsYCopyright} />
+        <Route path="*" component={Error404} />
       </Switch>
       <Footer />
     </BrowserRouter>
