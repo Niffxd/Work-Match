3cb94e157c5effdcd8d97cc306f9e711aaaf2975
Niffxd <nicosanchez673@gmail.com>
2023-02-07 17:25:23 -0300

    changing protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@
 
 import axios from "axios";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux"
+import { useAuth0 } from "@auth0/auth0-react";
 import Navigation from "./layout/Navigation/Navigation";
 import Footer from "./layout/Footer/Footer";
 import Home from "./pages/Home/Home";
@@ -16,15 +18,16 @@ import EmployerProfile from "./pages/UserProfile/EmployerProfile/EmployerProfile
 import DashboardUser from "./pages/UserProfile/UserProfile";
 import Error404 from "./pages/Error404/Error404";
 import Loading from "./components/Register/Loading";
-import { useAuth0 } from "@auth0/auth0-react";
 import EditJobOffer from "./pages/EditJobOffer/EditJobOffer";
 import AlertMessage from "./components/AlertMessage/AlertMessage";
 import TermsYCopyright from "./pages/TermsYCopyright/TermsYCopyright";
-import ProtectedRoute from "./components/protected-route"
+import LoaderPage from "./components/LoaderPage/LoaderPage"
+// import ProtectedRoute from "./components/protected-route"
 
 axios.defaults.baseURL = "http://localhost:3001";
 
 function App() {
+  const { user } = useSelector(state => state.user)
   const { isLoading } = useAuth0();
 
   if (isLoading) return <Loading />;
@@ -34,19 +37,36 @@ function App() {
       <AlertMessage />
       <Navigation />
       <Switch>
+        <Route exact path='/redirect' component={LoaderPage} />
         <Route exact path="/" component={Home} />
         <Route exact path="/home">
           <Redirect to="/" />
         </Route>
-
-     
-
         <Route exact path='/job-offer/:id' component={JobOfferDetail} />
         <Route exact path='/about-us' component={AboutUs} />
         <Route exact path='/Login' component={Login} />
         <Route exact path='/register' component={Register} />
         <Route exact path='/register/complete' component={PostUser} />
-        <ProtectedRoute exact path='/create-job-offer' component={CreateJobOffer} />
+
+        {/* Protected Routes */}
+        <Route exact path='/create-job-offer'>
+          {!Object.keys(user).length ? <Redirect to="/redirect" /> : <CreateJobOffer />}
+        </Route>
+        <Route exact path='/edit-job-offer'>
+          {!Object.keys(user).length ? <Redirect to="/redirect" /> : <EditJobOffer />}
+        </Route>
+        <Route exact path='/my-profile'>
+          {!Object.keys(user).length ? <Redirect to="/redirect" /> : <DashboardUser />}
+        </Route>
+        <Route exact path='/my-profile/employee/:type'>
+          {!Object.keys(user).length ? <Redirect to="/redirect" /> : <EmployeeProfile />}
+        </Route>
+        <Route exact path='/my-profile/employer/:type'>
+          {!Object.keys(user).length ? <Redirect to="/redirect" /> : <EmployerProfile />}
+        </Route>
+        {/* Protected Routes */}
+
+        {/* <ProtectedRoute exact path='/create-job-offer' component={CreateJobOffer} />
         <ProtectedRoute exact path='/edit-job-offer' component={EditJobOffer} />
         <ProtectedRoute exact path='/my-profile' component={DashboardUser} />
         <ProtectedRoute
@@ -58,7 +78,8 @@ function App() {
           exact
           path="/my-profile/employer/:type"
           component={EmployerProfile}
-        />
+        /> */}
+
         <Route exact path="/terms-and-copyright" component={TermsYCopyright} />
         <Route path="*" component={Error404} />
       </Switch>
